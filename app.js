a61d9ebdf72b1054b67c2fc03d0fd7801d8653da
Chibuike <aniakuchibuike@gmail.com>
2020-09-23 15:33:53 +0100

    setting up graphql

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import {graphqlHTTP} from 'express-graphql';
+import {buildSchema} from 'graphql'
 
 const app = express();
 
 app.use(bodyParser.json())
 
-app.get('/',(req, res, next) => {
-  res.send('Hello world!')
-})
+app.use(
+  '/graphql', 
+  graphqlHTTP({
+    schema: buildSchema(`
+
+        type Query {
+            events: [String!]!
+        }
+
+        type Mutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+          query: Query 
+          mutation: Mutation
+        }
+    `),
+    rootValue: {
+      events: () => {
+        return ['ball', 'cycle', 'party']
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName; 
+      }
+    },
+    graphiql: true
+}));
 
 app.listen(3000);
