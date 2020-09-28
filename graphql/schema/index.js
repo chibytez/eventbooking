661feee782706c4b39f35a7472642feb5f6a737d
Chibuike <aniakuchibuike@gmail.com>
2020-09-28 16:28:00 +0100

    adding dynamic relations

---
+++ graphql/schema/index.js
@@ -0,0 +1,47 @@
+import { buildSchema} from 'graphql';
+
+export default buildSchema(`
+        
+type Event {
+  _id: ID!
+  title: String!
+  description: String!
+  price: Float!
+  date: String!
+  creator: User!
+}
+
+type User {
+  _id: ID!
+  email: String!
+  password: String
+  createdEvents: [Event!]
+}
+
+input EventInput {
+  title: String!
+  description: String!
+  price: Float!
+  date: String!
+}
+
+input UserInput {
+  email: String!
+  password: String!
+
+}
+
+type Query {
+    events: [Event!]!
+}
+
+type Mutation {
+    createEvent(eventInput: EventInput): Event
+    createUser(userInput: UserInput): User
+  }
+
+schema {
+  query: Query 
+  mutation: Mutation
+}
+`)
