cfe4513eed668dfc46905429cfd373b17b2f0061
Chibuike <aniakuchibuike@gmail.com>
2020-10-10 17:12:09 +0100

    adding dataloader

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -3,9 +3,16 @@
 /* eslint-disable prefer-const */
 /* eslint-disable no-useless-catch */
 /* eslint-disable no-shadow */
+import DataLoader from 'dataloader';
 import Event from '../../models/event';
 import User from '../../models/user';
 import { dateToString } from '../../helpers/date';
+
+const eventLoader = new DataLoader(eventIds => events(eventIds));
+
+const userLoader = new DataLoader((userIds) => {
+  return User.find({ _id: { $in: userIds } });
+});
 
 const events = async eventIds => {
   try {
@@ -18,8 +25,8 @@ const events = async eventIds => {
 
 const singleEvent = async eventId => {
   try {
-    const event = await Event.findById(eventId);
-    return transformEvent(event);
+    const event = await eventLoader.load(eventId.toString());
+    return event;
   } catch (err) {
     throw err;
   }
@@ -27,10 +34,10 @@ const singleEvent = async eventId => {
 
 const user = async userId => {
   try {
-    const user = await User.findById(userId);
+    const user = await userLoader.load(userId.toString());
     return {
       ...user._doc,
-      createdEvents: events.bind(this, user._doc.createdEvents)
+      createdEvents: eventLoader.loadMany.bind(this, user._doc.createdEvents)
     };
   } catch (err) {
     throw err;
@@ -40,7 +47,7 @@ const user = async userId => {
 const transformEvent = event => ({
   ...event._doc,
   date: dateToString(event._doc.date),
-  creator: user.bind(this, event.creator),
+  creator: user.bind(this, event.creator)
 });
 
 const transformBooking = booking => ({
@@ -48,7 +55,7 @@ const transformBooking = booking => ({
   user: user.bind(this, booking._doc.user),
   event: singleEvent.bind(this, booking._doc.event),
   createdAt: dateToString(booking._doc.createdAt),
-  updatedAt: dateToString(booking._doc.createdAt),
+  updatedAt: dateToString(booking._doc.createdAt)
 });
 
 export { transformEvent, transformBooking };
